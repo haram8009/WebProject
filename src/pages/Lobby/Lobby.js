962f6e2869e1e0a42a1372f65243323f05f704b6
haram8009 <haram8009@gmail.com>
2023-02-03 11:23:56 +0900

    로비 수정

--- src/pages/Lobby/Lobby.js
+++ src/pages/Lobby/Lobby.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { ContentWrap, Wrap } from "./styles";
 
 function Lobby() {
   return (
-    <>
+    <Wrap>
       <h1>Team.계람.</h1>
-    </>
+      <ContentWrap>
+        <div>가나다</div>
+        <div>abc</div>
+      </ContentWrap>
+    </Wrap>
   );
 }
 export default Lobby;
