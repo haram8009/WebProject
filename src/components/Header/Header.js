83cf65368b8a115011001c71abe909541e694824
haram8009 <haram8009@gmail.com>
2023-02-03 11:12:32 +0900

    header footer logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,21 @@
 import React from "react";
-import { Wrap } from "./styles";
+import { Left, Right, Wrap } from "./styles";
 
 function Header() {
-  return <Wrap>this is header</Wrap>;
+  return (
+    <Wrap>
+      <div>
+        <Left>
+          <img
+            src={process.env.PUBLIC_URL + "/images/common/zeppelin.gif"}
+            alt="zeppelin gif from https://www.flaticon.com/kr/free-animated-icons"
+            title="제플린 애니메이션 아이콘 제작자: Freepik - Flaticon"
+          />
+          <span>this is header</span>
+        </Left>
+        <Right></Right>
+      </div>
+    </Wrap>
+  );
 }
 export default Header;
--- src/pages/Lobby/Lobby.js
+++ src/pages/Lobby/Lobby.js
@@ -1,6 +1,10 @@
 import React from "react";
 
 function Lobby() {
-  return <h1>Team.계람.</h1>;
+  return (
+    <>
+      <h1>Team.계람.</h1>
+    </>
+  );
 }
 export default Lobby;
